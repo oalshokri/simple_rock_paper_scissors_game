0d52ec1967e7f603fe213610e6d434626bb31a54
oalshokri <janataanfor@gmail.com>
2022-12-09 12:28:13 +0200

    - add state to app - pick random image

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,17 +11,58 @@ void main() {
           title: const Text('Rock Paper Scissors'),
           backgroundColor: Colors.indigo.shade900,
         ),
-        body: const GamePage(),
+        body: GamePage(),
       ),
     ),
   );
 }
 
-class GamePage extends StatelessWidget {
-  const GamePage({super.key});
+class GamePage extends StatefulWidget {
+  const GamePage({Key? key}) : super(key: key);
+
+  @override
+  State<GamePage> createState() => _GamePageState();
+}
+
+class _GamePageState extends State<GamePage> {
+  List<String> images = ['paper_btn', 'rock_btn', 'scisor_btn'];
+
+  late String name;
+  late String robot;
+
+  @override
+  void initState() {
+    name = images[0];
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    robot = images[Random().nextInt(3)];
+    return Center(
+      child: Column(
+        children: [
+          CircleAvatar(),
+          Row(
+            children: [
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    print(name);
+                    setState(() {
+                      name = images[Random().nextInt(3)];
+                    });
+                  },
+                  child: Image.asset('images/$name.png'),
+                ),
+              ),
+              Expanded(
+                child: Image.asset('images/$robot.png'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
